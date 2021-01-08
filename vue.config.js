b9ecab6bc7f6b33326e9c3c7894c8aa5eb15cafd
msli121 <2276695595@qq.com>
2021-01-08 23:21:47 +0800

    update

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServe: {
+    proxy: {
+      // api是后端数据接口的地址
+      'api': {
+        // 后端主机
+        target: "http://111.229.37.167/",
+        // 允许跨域
+        changOrigin: true
+      }
+    }
+  }
+}
