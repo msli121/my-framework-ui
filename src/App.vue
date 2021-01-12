4ae83f00e5a95f7eaba9f6debc4e7bedf7e323c1
msli121 <2276695595@qq.com>
2021-01-12 14:03:23 +0800

    update 2020-01-12/3

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <div id="app">
     <!-- 路由的出入口，路由的内容将被显示在这里 -->
     <router-view></router-view>
-    <!-- <router-link to="/"></router-link> -->
-    <!-- <home></home> -->
   </div>
 </template>
 
